b75c1ab3b14ebb2973a36f0b442e03b32b248760
otothis <othmane.gb00@gmail.com>
2024-07-04 09:36:34 +0100

    add app

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
           </span>
         </li>
       </a>
-      <a href="https://www.linkedin.com/in/luizeduardomr/" target="_blank">
+      <a href="https://drive.google.com/file/d/1ZcdDAPOTJWDM7FV2_y--kGFxh1uxRvah/view?usp=drive_link" target="_blank">
         <li style={{background: 'linear-gradient(to right, #0866C2, #044d96)'}}>
           {/* <i data-feather="linkedin"></i> */}
           <span>
@@ -21,7 +21,7 @@ export default function Home() {
           </span>
         </li>
       </a>
-      <a href="https://discord.bio/p/luiz" target="_blank">
+      <a href="https://drive.google.com/file/d/1g60n-LDrTnqhHJglthGUlfZPxBlaX_mZ/view?usp=drive_link" target="_blank">
         <li style={{background: 'linear-gradient(to right, #2673ca, #346485)'}}>
           <span>
             ByMap App
